84f7ffa73fb2ed9026402c71e910981085712671
Sam Bitten <78203808+sambitten@users.noreply.github.com>
2021-11-06 10:54:57 +0000

    ex6 completed

--- Exercise6.hs
+++ Exercise6.hs
@@ -52,5 +52,77 @@ concat' [x] = x
 concat' (x:xs) = x ++ (concat' xs)
 
 
+replicate' :: Int -> a -> [a]
+replicate' 0 x = []
+replicate' n x = [x] ++ replicate' (n-1) x 
+
+
+select' :: [a] -> Int -> a
+select' (x:xs) 0 = x 
+select' (x:xs) n = select' xs (n-1)
+
+
+elem' :: Eq a => a -> [a] -> Bool
+elem' n [] = False 
+elem' n (x:xs)| n == x    = True
+			  | otherwise = elem' n xs
+			  
+			  
+
+--4.
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge (x:xs) (y:ys) = if x < y
+                        then x:(merge xs (y:ys))
+                        else y:(merge (x:xs) ys)
+merge [] xs = xs
+merge xs [] = xs 
+
+--merge [2,5,6] [1,3,4]
+--1:(merge ([2,5,6]) [3,4])
+--1:(2:(merge [5,6] [3,4])
+--1:(2:(3: merge [5,6] [4])
+--1:(2:(3:(4:( merge [5,6] [])
+--1:(2:(3:(4:([5,6])
+--[1,2,3,4,5,6]
+
+
+
+
+--5.
+
+halve :: [a] -> ([a],[a])
+halve xs = ((take n xs),(drop n xs))
+		   where n = length xs `div` 2
+		   
+
+msort :: Ord a => [a] -> [a]
+msort []  = []
+msort [x] = [x]
+msort  xs = merge (msort left) (msort right)
+            where (left,right) = halve xs
+		   	
+
+
+
+--6.
+
+--calculate sum of a list
+listSum :: [Int] -> Int
+listSum [] = 0
+listSum (x:xs) = x + (listSum xs) 
+
+--take given number of elements from start of a list
+takeList :: Int -> [a] -> [a]
+takeList n [] = []
+takeList 0 xs = xs 
+takeList n (x:xs) = takeList (n-1) xs 
+
+--select last element from a non empty list
+selectLast :: [a] -> a
+selectLast [x] = x
+selectLast (x:xs) = selectLast xs 
+
+
 
 
