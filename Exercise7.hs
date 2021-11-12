2a04f67defa9c1828430cf1584e98c9192538014
Sam Bitten <78203808+sambitten@users.noreply.github.com>
2021-11-12 22:35:51 +0000

    up to Q2 part iii

--- Exercise7.hs
+++ Exercise7.hs
@@ -1,1 +1,16 @@
 -- chapter 7 exercises - higher order functions
+
+
+--1.  write [f x | x <- xs, p x] as a higher order function (map/filter)
+
+question1 p xs = filter p xs
+
+
+
+--2.
+
+all' :: (a -> Bool) -> [a] -> Bool
+all' p = and . map p 
+
+any' :: (a -> Bool) -> [a] -> Bool
+any' p = or . map p  
