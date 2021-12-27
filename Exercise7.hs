875dd6bfc4a8fba990988582d780562bd05b20bb
Sam Bitten <78203808+sambitten@users.noreply.github.com>
2021-12-27 22:14:05 +0000

    Update Exercise7.hs

--- Exercise7.hs
+++ Exercise7.hs
@@ -3,7 +3,7 @@
 
 --1.  write [f x | x <- xs, p x] as a higher order function (map/filter)
 
-question1 p xs = filter p xs
+question1 f xs = filter f xs
 
 
 
@@ -13,4 +13,33 @@ all' :: (a -> Bool) -> [a] -> Bool
 all' p = and . map p 
 
 any' :: (a -> Bool) -> [a] -> Bool
-any' p = or . map p  
+any' p = or . map p 
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' f [] = []
+takeWhile' f (x:xs) | f x 	    = x:takeWhile' f xs
+					| otherwise = []
+					
+					
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' p [] = []
+dropWhile' p (x:xs)
+  | p x = dropWhile' p xs
+  | otherwise = x : xs
+
+
+--3.       redefine map and filter using foldr
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x xs -> f x : xs) []
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f =
+  foldr
+    (\x xs ->
+       if f x
+         then x : xs
+       else xs)
+    []
+
+
