eef105327a6ccf192a4caf1e2f6ad6d76981e569
Sam Bitten <78203808+sambitten@users.noreply.github.com>
2021-12-30 15:47:37 +0000

    Update Exercise7.hs

--- Exercise7.hs
+++ Exercise7.hs
@@ -1,5 +1,7 @@
 -- chapter 7 exercises - higher order functions
 
+import Data.List
+
 
 --1.  write [f x | x <- xs, p x] as a higher order function (map/filter)
 
@@ -41,5 +43,97 @@ filter' f =
          then x : xs
        else xs)
     []
+	
+	
+--4. define a function that converts a list of ints to an int eg dec2int [2,3,4,5] = 2345
+
+--dec2int :: [Int] -> Int
+--dec2int = foldl (\n d -> 10 * n + d) 0
+
+
+--5. 
+curry' :: ((a, b) -> c) -> a -> b -> c
+curry' f = \a b -> f (a, b)
+
+uncurry' :: (a -> b -> c) -> (a, b) -> c
+uncurry' f = \(a, b) -> f a b
+
+
+--7.
+type Bit = Int
+
+unfold :: (t -> Bool) -> (t -> a) -> (t -> t) -> t -> [a]
+unfold p h t x
+  | p x = []
+  | otherwise = h x : unfold p h t (t x)
+
+int2bin :: Int -> [Bit]
+int2bin = unfold (== 0) (`mod` 2) (`div` 2)
+
+chop8 :: [Bit] -> [[Bit]]
+chop8 = unfold null (take 8) (drop 8)
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = unfold null (f . head) tail
+
+iterate' :: (a -> a) -> a -> [a]
+iterate' = unfold (const False) id
+
+
+--8.
+import Data.Char
+
+type Bit = Int
+
+bin2int :: [Bit] -> Int
+bin2int bits = sum [w * b | (w, b) <- zip weights bits]
+  where
+    weights = iterate (* 2) 1
+
+bin2int' :: [Bit] -> Int
+bin2int' = foldr (\x y -> x + 2 * y) 0
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+make8 :: [Bit] -> [Bit]
+make8 bits = take 8 (bits ++ repeat 0)
+
+parity :: [Bit] -> Bit
+parity bits = sum bits `mod` 2
+
+addParity :: [Bit] -> [Bit]
+addParity bits = parity bits : bits
+
+encode :: String -> [Bit]
+encode = concatMap (addParity . make8 . int2bin . ord)
+
+chop9 :: [Bit] -> [[Bit]]
+chop9 [] = []
+chop9 bits = take 9 bits : chop9 (drop 9 bits)
+
+checkParity :: [Bit] -> [Bit]
+checkParity bits
+  | head bits == parity (tail bits) = tail bits
+  | otherwise = error "Parity Error"
+
+decode :: [Bit] -> String
+decode = map (chr . bin2int . checkParity) . chop9
+
+channel :: [Bit] -> [Bit]
+channel = tail
+
+transmit :: String -> String
+transmit = decode . channel . encode
+
+
+--9.
+altMap :: (a -> b) -> (a -> b) -> [a] -> [b]
+altMap _ _ [] = []
+altMap f0 f1 (x:xs) = f0 x : altMap f1 f0 xs
+
+
+
 
 
